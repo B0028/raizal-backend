a2fc7f42addbf6f470fde086106f6cdbe9d01c4d
Baz003 <226179469+B0028@users.noreply.github.com>
2026-06-27 21:53:06 -0300

    fix(cors): solucion a deploy en vercel

--- src/config/cors.js
+++ src/config/cors.js
@@ -2,6 +2,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "https://raizal-hydroponics.vercel.app",
+        "https://raizal-1afxzz5do-satorubakzy-2774s-projects.vercel.app",
         "https://raizal.up.railway.app"
     ],
     credentials: true
